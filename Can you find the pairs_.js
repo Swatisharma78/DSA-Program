8deee726b1ff2774698d37ac46637819c969ebc8
SWATI SHARMA <99734340+Swatisharma78@users.noreply.github.com>
2023-01-13 22:12:28 +0530

    dsa

---
+++ Can you find the pairs_.js
@@ -0,0 +1,38 @@
+/*Description
+
+Given an array of integers, find all pairs (i,j) such that 2*a[i] = 3*a[j] and i != j
+
+
+Input
+Input Format
+
+The first line contains T, the number of test cases
+
+The first line of each testcase contains n, the size of the array
+
+The second line of each testcase contains n space separated integers
+
+Constraints
+
+1 <= T <= 10
+
+1 <= n <= 10^5
+
+-3000 <= a[i] <= 3000
+
+
+Output
+For each testcase, print the number of possible pairs on a new line
+
+
+Sample Input 1 
+
+2
+6
+6 6 6 4 4 4
+4
+0 0 0 0
+Sample Output 1
+
+9
+6*/
